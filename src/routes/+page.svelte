b6ad27eed947f3b47742e0b7672b4ec45a86b9a2
Michał Szczepański <siematumichael@protonmail.com>
2023-10-03 20:31:41 +0200

    Enrolled Function covering needance of application point deletion

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
     import * as fs from "@tauri-apps/api/fs";
     import { goto } from "$app/navigation"
     import { saveFile, store } from "$lib/api";
+    import { TrashCan } from "carbon-icons-svelte";
 
     // Load data to list 
     onMount(async () => {
@@ -19,6 +20,18 @@
     $: if ($store.length) {
         saveFile();
     }
+
+    /**
+     * @description Delete point from TODO list
+     * @param i Index of point to delete
+    */
+    function deletePoint(i: number) {
+        return () => {
+            $store.splice(i, 1);
+            $store = $store; // Register deletion action
+            saveFile(); // Save file with took into account deleted item
+        }
+    }
 </script>
 
 <div class="flex justify-between items-center">
@@ -29,12 +42,15 @@
 </div>
 <div class="list mt-4">
     {#each $store as point, i (i)}
-        <label class="flex items-center space-x-2">
+        <label class="w-full flex items-center space-x-2 relative">
             <input class="checkbox" type="checkbox" checked={point.done} on:change={v => {
                 console.log(v.currentTarget.value)
                 $store[i].done = Boolean(v.currentTarget.checked);
             }}/>
             <p>{point.name}</p>
+            <button class="p-2 absolute right-0" on:click={deletePoint(i)}>
+                <TrashCan size={20} fill="white"/>
+            </button>
         </label>
     {/each}
 </div>
